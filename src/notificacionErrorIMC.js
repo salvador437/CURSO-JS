f5814b5bc00a2db963a4446c7461b6f1dc266d6d
salvador <salva@gmail,com>
2024-11-17 23:00:20 +0100

    corregidos problemas paginas de error

--- src/notificacionErrorIMC.js
+++ src/notificacionErrorIMC.js
@@ -1,9 +1,9 @@
 // Obtener elementos del DOM
 const modal = document.getElementById("myModal");
 // const openModalBtn = document.getElementById("openModalBtn");
-const closeBtn = document.getElementsByClassName("close")[0];
+const closeBtn = document.getElementsByClassName("close2")[0];
 
 closeBtn.onclick = function () {
   modal.style.display = "none";
-  open("./index.htm", target="_self"   );
+  open("./pagina2.html", target="_self");
 };
